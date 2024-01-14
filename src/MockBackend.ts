2d2045898f37e7631fbfac0074f454438e241a48
MatthewGalan <mattg54321@gmail.com>
2024-01-13 18:47:40 -0800

    add backend skeleton functions

--- src/MockBackend.ts
+++ src/MockBackend.ts
@@ -1,17 +1,45 @@
-export const User = {};
+export interface User {
+  email: string;
+  username: string;
+  displayName: string;
+  avatar: string;
+}
 
-export function getUser() {
+export interface Post {
+  id: string;
+  author: string;
+  dateTime: string;
+  text: string;
+  replies: Post[];
+}
+
+export function getUser(): User | undefined {
   // return undefined;
 
   return {
+    email: "mattg@example.com",
+    username: "mattg5",
+    displayName: "Matt G",
     avatar: "https://avatars.githubusercontent.com/u/20482179?v=4",
   };
 }
 
-export function registerUser(user: {
+export function registerUser(newUser: {
   email: string;
   username: string;
   displayName: string;
 }) {
-  return user;
+  return {
+    ...newUser,
+    avatar: "https://avatars.githubusercontent.com/u/20482179?v=4",
+  };
 }
+
+// if parentId is provided, it's a reply to that post. otherwise it's a new post
+export function createPost(text: string, parentId?: string) {}
+
+export function editPost(id: string, text: string) {}
+
+export function deletePost(id: string) {}
+
+export function getFeed() {}
