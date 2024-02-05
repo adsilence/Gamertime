9a3b73a78b62f41dda8b29c6284b7f86de4202f0
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2024-02-05 00:58:14 -0500

    Use bun:sqlite instead of better-sqlite3

--- src/db/clients/database.ts
+++ src/db/clients/database.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
 
 console.log("Connecting to database!");
 
--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -1,6 +1,7 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
-import Database from 'better-sqlite3';
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
 
 const sqliteDbConnection = new Database('sqlite.db');
 
